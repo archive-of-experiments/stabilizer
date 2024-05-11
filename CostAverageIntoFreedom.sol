5a502ffba297ec4d45aa5610706629d8856860a0
Monique Baumann <160405077+moniquebaumann@users.noreply.github.com>
2024-05-11 13:52:10 +0200

    Update CostAverageIntoFreedom.sol

--- CostAverageIntoFreedom.sol
+++ CostAverageIntoFreedom.sol
@@ -11,16 +11,13 @@
 // We foster ever emerging architectures of freedom by rewarding those who help themselves and others to be free.
 
 pragma solidity 0.8.19;
-import "https://raw.githubusercontent.com/moniquebaumann/freedomswaps/v0.0.1/IFreedomSwaps.sol";
-import "https://raw.githubusercontent.com/Uniswap/v3-periphery/main/contracts/interfaces/ISwapRouter.sol";
-import "https://raw.githubusercontent.com/Uniswap/solidity-lib/master/contracts/libraries/TransferHelper.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.4/contracts/token/ERC20/IERC20.sol";
 
+import "https://raw.githubusercontent.com/moniquebaumann/freedomswaps/v1.0.0/IFreedomSwaps.sol";
 
 contract CostAverageIntoFreedom {
 
     address private constant MATIC          = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    address private constant FREEDOMSWAPS   = 0xA70f5023801F06A6a4C04695E794cf6e2ecCb34F;
+    address private constant FREEDOMSWAPS   = 0x06b80F66A1A72a9C36FB0298EE1B22406e377d0e;
     address private constant SWAP_ROUTER    = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     mapping(address => IDeposit) public deposits;
     struct IDeposit {
@@ -30,22 +27,17 @@ contract CostAverageIntoFreedom {
         uint256 minInterval; // e.g. 36000 (assuming a block-time of 2 seconds this would be around 5 hours)
         uint256 lastPurchase; // block number at last purchase or initial
         uint256 perPurchaseAmount; // defines how much Matic goes into each purchase
-        uint256 claimable; // represents the amount the Freedom Lover can claim
+        address payable recipient; // the beneficiary
     }
-    ISwapRouter public immutable swapRouter;
 
     error Wait();
     error CheckInput();
     error DepositAlreadySwapped();
 
-    constructor() {
-        swapRouter = ISwapRouter(SWAP_ROUTER);
-    }
-
-    function deposit(uint256 minInterval, uint256 perPurchaseAmount, address token) public payable {
+    function deposit(uint256 minInterval, uint256 perPurchaseAmount, address token, address payable recipient) public payable {
         if ((msg.value % perPurchaseAmount) != 0) { revert CheckInput(); }
-        if (deposits[msg.sender].input - deposits[msg.sender].swapped == 0 && deposits[msg.sender].claimable == 0) {
-            deposits[msg.sender] = IDeposit(msg.value, token, 0, minInterval, 0, perPurchaseAmount, 0);
+        if (deposits[msg.sender].input - deposits[msg.sender].swapped == 0) {
+            deposits[msg.sender] = IDeposit(msg.value, token, 0, minInterval, 0, perPurchaseAmount, recipient);
         } else if(token == deposits[msg.sender].token) {
             deposits[msg.sender].input += msg.value;
             deposits[msg.sender].minInterval = minInterval;
@@ -58,24 +50,14 @@ contract CostAverageIntoFreedom {
     function trigger(uint24 poolFee, uint24 slippage) public {
         if (deposits[msg.sender].input >= deposits[msg.sender].swapped + deposits[msg.sender].perPurchaseAmount) {
             if (block.number > deposits[msg.sender].lastPurchase + deposits[msg.sender].minInterval) {
-                uint256 amountOut = IFreedomSwaps(FREEDOMSWAPS).swapBaseCurrency{value: deposits[msg.sender].perPurchaseAmount}(MATIC, deposits[msg.sender].token, poolFee, slippage);
+                IFreedomSwaps(FREEDOMSWAPS).swapBaseCurrency{value: deposits[msg.sender].perPurchaseAmount}(MATIC, deposits[msg.sender].token, deposits[msg.sender].recipient, poolFee, slippage);
                 deposits[msg.sender].lastPurchase = block.number;
                 deposits[msg.sender].swapped += deposits[msg.sender].perPurchaseAmount;
-                deposits[msg.sender].claimable += amountOut;
             } else {
                 revert Wait();
             }
         } else {
             revert DepositAlreadySwapped();
         }
-    }
-
-    function claim() public {
-        if (deposits[msg.sender].claimable == 0) { revert CheckInput(); }
-        if (IERC20(deposits[msg.sender].token).allowance(address(this), msg.sender) < deposits[msg.sender].claimable) {
-            TransferHelper.safeApprove(deposits[msg.sender].token, msg.sender, deposits[msg.sender].claimable);
-        }
-        IERC20(deposits[msg.sender].token).transferFrom(address(this), msg.sender, deposits[msg.sender].claimable);        
-        deposits[msg.sender].claimable = 0;
     }
 }
