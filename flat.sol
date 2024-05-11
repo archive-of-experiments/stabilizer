3af84c63d67a6f4470f74660c3b19b1a118e90a6
Monique Baumann <160405077+moniquebaumann@users.noreply.github.com>
2024-05-11 20:51:13 +0200

    Update flat.sol

--- flat.sol
+++ flat.sol
@@ -29,7 +29,6 @@ interface IFreedomSwaps {
 // File: cost-average-into-freedom.sol
 
 
-
 // We fund freedom.
 // We stop state criminals.
 // We make crypto cypherpunk again.
@@ -48,45 +47,53 @@ contract CostAverageIntoFreedom {
     address private constant MATIC          = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     address private constant FREEDOMSWAPS   = 0x06b80F66A1A72a9C36FB0298EE1B22406e377d0e;
     address private constant SWAP_ROUTER    = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    mapping(address => IDeposit) public deposits;
+
+    uint256 public depositsCounter = 0;
+
+    mapping(uint256 => IDeposit) public deposits; 
+    
+    mapping(address => uint256[]) depositIDs; 
+
     struct IDeposit {
+        address from; // msg.sender of deposit
         uint256 input; // contains the amount of Matic deposited 
         address token; // e.g. 0xb841A4f979F9510760ecf60512e038656E68f459 
         uint256 swapped; // contains the total amount of Matic already swapped 
         uint256 minInterval; // e.g. 36000 (assuming a block-time of 2 seconds this would be around 5 hours)
         uint256 lastPurchase; // block number at last purchase or initial
-        uint256 perPurchaseAmount; // defines how much Matic goes into each purchase
-        address payable recipient; // the beneficiary
+        address payable[] recipients; // the beneficiaries 
+        uint256 perPurchasePerRecipientAmount; // defines how much Matic goes into each purchase per recipient
     }
 
     error Wait();
     error CheckInput();
     error DepositAlreadySwapped();
 
-    function deposit(uint256 minInterval, uint256 perPurchaseAmount, address token, address payable recipient) public payable {
-        if ((msg.value % perPurchaseAmount) != 0) { revert CheckInput(); }
-        if (deposits[msg.sender].input - deposits[msg.sender].swapped == 0) {
-            deposits[msg.sender] = IDeposit(msg.value, token, 0, minInterval, 0, perPurchaseAmount, recipient);
-        } else if(token == deposits[msg.sender].token) {
-            deposits[msg.sender].input += msg.value;
-            deposits[msg.sender].minInterval = minInterval;
-            deposits[msg.sender].perPurchaseAmount = perPurchaseAmount;
-        } else {
-            revert CheckInput();
-        }
+    function deposit(uint256 minInterval, uint256 perPurchasePerRecipientAmount, address token, address payable[] memory recipients) public payable {
+        if (msg.value % recipients.length != 0) { revert CheckInput(); }
+        deposits[depositsCounter] = IDeposit(msg.sender, msg.value, token, 0, minInterval, 0, recipients, perPurchasePerRecipientAmount);
+        depositIDs[msg.sender].push(depositsCounter);
+        depositsCounter++;
     }
 
-    function trigger(uint24 poolFee, uint24 slippage) public {
-        if (deposits[msg.sender].input >= deposits[msg.sender].swapped + deposits[msg.sender].perPurchaseAmount) {
-            if (block.number > deposits[msg.sender].lastPurchase + deposits[msg.sender].minInterval) {
-                IFreedomSwaps(FREEDOMSWAPS).swapBaseCurrency{value: deposits[msg.sender].perPurchaseAmount}(MATIC, deposits[msg.sender].token, deposits[msg.sender].recipient, poolFee, slippage);
-                deposits[msg.sender].lastPurchase = block.number;
-                deposits[msg.sender].swapped += deposits[msg.sender].perPurchaseAmount;
+    function trigger(uint256 depositID, uint24 poolFee, uint24 slippage) public {
+        if (deposits[depositID].from != msg.sender) { revert CheckInput(); }
+        if (deposits[depositID].input > deposits[depositID].swapped) {
+            if (block.number > deposits[depositID].lastPurchase + deposits[depositID].minInterval) {
+                for (uint256 i = 0; i < deposits[depositID].recipients.length; i++) {
+                    IFreedomSwaps(FREEDOMSWAPS).swapBaseCurrency{value: deposits[depositID].perPurchasePerRecipientAmount}(MATIC, deposits[depositID].token, deposits[depositID].recipients[i], poolFee, slippage);
+                }
+                deposits[depositID].lastPurchase = block.number;
+                deposits[depositID].swapped += deposits[depositID].perPurchasePerRecipientAmount * deposits[depositID].recipients.length;
             } else {
                 revert Wait();
             }
         } else {
             revert DepositAlreadySwapped();
         }
+    }
+
+    function getDepositIDs() public view returns(uint256[] memory) {
+        return depositIDs[msg.sender];
     }
 }
